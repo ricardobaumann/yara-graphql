d9bbb19d34be5003e36aca655fc85b1f7921cb82
ricardobaumann <gemini.richard@gmail.com>
2024-03-22 15:48:51 +0100

    Improving Product service unit test

--- tests/product-service.test.js
+++ tests/product-service.test.js
@@ -1,4 +1,4 @@
-import { expect, test, vi, describe, it, beforeAll, afterAll } from "vitest";
+import { expect, vi, describe, it, beforeAll } from "vitest";
 import prisma from '../db/__mocks__/prisma'
 vi.mock('../db/prisma')
 import {ProductService} from "../service/product/index";
@@ -16,7 +16,7 @@ describe("Product Service", ()=> {
             productType: "bar",
             id: crypto.randomUUID().toString()
         };
-        prisma.product.create.mockResolvedValue({...newProduct})
+        prisma.product.create.mockResolvedValue({...newProduct});
         const product = await productService.addProduct(newProduct.productName, newProduct.productType);
         expect(product).toStrictEqual({ ...newProduct })
         const data = prisma.product.create.mock.calls[0][0].data;
